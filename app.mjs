0b99b93a72b06415fb300ec9b9a56bf53c06a2e1
George Anya <georgeanya4real@gmail.com>
2022-07-28 20:23:13 +0100

    commit

--- app.mjs
+++ app.mjs
@@ -8,13 +8,41 @@ function startApp() {
   const myInput = document.getElementById("my-num-input");
   const myCountry = document.getElementById("my-country-input");
   const myName = document.getElementById("my-name-input");
-  const container = document.querySelector(".mtn-container");
-  myInput.addEventListener("input", function (e) {
+  const button = document.getElementById("submit");
+  const button1 = document.getElementById("submitt");
+  const container = document.querySelector(".img-container");
+  const prefix = document.querySelector(".prefix");
+  const netResult = document.getElementById("results");
+  const netForm = document.getElementById("input-form");
+  
+  netResult.style.display = 'none'
+
+  myCountry.addEventListener("change", function (e) {
+    console.log(e);
+    if (myCountry.value === "nig") {
+      prefix.innerHTML = "+234";
+    } else if (myCountry.value === "ken") {
+      prefix.innerHTML = "+254";
+    } else if (myCountry.value === "cam") {
+      prefix.innerHTML = "+237";
+    } else if (myCountry.value === "gha") {
+      prefix.innerHTML = "+233";
+      // } else if ((myCountry.value = "cam")) {
+      //   document.getElementById("prefix").innerHTML = "+234";
+    } else {
+      console.log("error");
+    }
+  });
+
+  button.addEventListener("click", function (e) {
     e.preventDefault();
     let text = myInput.value.toString();
+    netForm.style.display = 'none'
+    netResult.style.display = 'block'
+
     if (
-      (myCountry.value =
-        "nig" & text.includes("803") ||
+      (myCountry.value === "nig") &
+      (text.includes("803") ||
         text.includes("706") ||
         text.includes("7025") ||
         text.includes("7026") ||
@@ -28,109 +56,110 @@ function startApp() {
         text.includes("906") ||
         text.includes("903"))
     ) {
-      let netImage = `<img src="https://res.cloudinary.com/seyitunde/image/upload/v1658828502/mtn.png" alt="mtn logo" id="mtn-logo"></img>`;
+      let netImage = `<img src=".//images/mtn.png" alt="mtn logo" id="mtn-logo"></img>`;
       container.innerHTML = netImage;
       document.getElementById("demo").innerHTML =
         "Hi " + myName.value + ", your network service provider is MTN Nigeria";
     } else if (
-      (myCountry.value =
-        "nig" & text.includes("805") ||
+      (myCountry.value === "nig") &
+      (text.includes("805") ||
         text.includes("807") ||
         text.includes("705") ||
         text.includes("815") ||
         text.includes("811") ||
         text.includes("905"))
     ) {
-      let netImage = `<img src="https://res.cloudinary.com/seyitunde/image/upload/v1658828503/glo.png" id="glo-logo"></img>`;
+      let netImage = `<img src=".//images/glo.jpg" id="glo-logo"></img>`;
       container.innerHTML = netImage;
       document.getElementById("demo").innerHTML =
         "Hi " +
         myName.value +
         ", your network  service provider is GLO Nigeria";
     } else if (
-      (myCountry.value =
-        "nig" & text.includes("802") ||
+      (myCountry.value === "nig") &
+      (text.includes("802") ||
         text.includes("808") ||
         text.includes("708") ||
         text.includes("812") ||
         text.includes("701") ||
         text.includes("902"))
     ) {
-      let netImage = `<img src="https://res.cloudinary.com/seyitunde/image/upload/v1658828507/airtel.png"airtel logo" id="airtel-logo"></img>`;
+      let netImage = `<img src=".//images/air.png"airtel logo" id="airtel-logo"></img>`;
       container.innerHTML = netImage;
       document.getElementById("demo").innerHTML =
         "Hi " +
         myName.value +
         ", your network  service provider is Airtel Nigeria";
     } else if (
-      (myCountry.value =
-        "nig" & text.includes("809") ||
+      (myCountry.value === "nig") &
+      (text.includes("809") ||
         text.includes("818") ||
         text.includes("817") ||
         text.includes("909"))
     ) {
-      let netImage = `<img src="https://res.cloudinary.com/seyitunde/image/upload/v1658828503/ninemobile.png" alt="9mobile logo" id="ninemobile-logo"></img>`;
+      let netImage = `<img src=".//images/9m.png" alt="9mobile logo" id="9mobile-logo"></img>`;
       container.innerHTML = netImage;
       document.getElementById("demo").innerHTML =
         "Hi " +
         myName.value +
         ", your network  service provider is 9mobile Nigeria";
     } else if (
-      (myCountry.value =
-        "gha" & text.includes("024") ||
+      (myCountry.value === "gha") &
+      (text.includes("024") ||
         text.includes("054") ||
         text.includes("055") ||
         text.includes("059"))
     ) {
-      let netImage = `<img src="https://res.cloudinary.com/seyitunde/image/upload/v1658828503/ninemobile.png" alt="9mobile logo" id="ninemobile-logo"></img>`;
+      let netImage = `<img src=".//images/mtn.png" alt="mtn logo" ></img>`;
       container.innerHTML = netImage;
       document.getElementById("demo").innerHTML =
         "Hi " + myName.value + ", your network  service provider is MTN Ghana";
     } else if (
-      (myCountry.value =
-        "gha" & text.includes("026") ||
+      (myCountry.value === "gha") &
+      (text.includes("026") ||
         text.includes("056") ||
         text.includes("027") ||
         text.includes("057"))
     ) {
-      let netImage = `<img src="https://res.cloudinary.com/seyitunde/image/upload/v1658828503/ninemobile.png" alt="9mobile logo" id="ninemobile-logo"></img>`;
+      let netImage = `<img src=".//images/tigo.png" alt="tigo logo"></img>`;
       container.innerHTML = netImage;
       document.getElementById("demo").innerHTML =
         "Hi " + myName.value + ", your network  service provider is Tigo Ghana";
     } else if (
-      (myCountry.value = "gha" & text.includes("020") || text.includes("050"))
+      (myCountry.value === "gha") &
+      (text.includes("020") || text.includes("050"))
     ) {
-      let netImage = `<img src="https://res.cloudinary.com/seyitunde/image/upload/v1658828503/ninemobile.png" alt="9mobile logo" id="ninemobile-logo"></img>`;
+      let netImage = `<img src=".//images/voda.png" alt="vodafone logo" ></img>`;
       container.innerHTML = netImage;
       document.getElementById("demo").innerHTML =
         "Hi " +
         myName.value +
         ", your network  service provider is Vodafone Ghana";
-    } else if ((myCountry.value = "nig" & text.includes("028"))) {
-      let netImage = `<img src="https://res.cloudinary.com/seyitunde/image/upload/v1658828503/ninemobile.png" alt="9mobile logo" id="ninemobile-logo"></img>`;
+    } else if ((myCountry.value === "gha") & (text.includes("028"))) {
+      let netImage = `<img src=".//images/exp.png" alt="expresso logo"></img>`;
       container.innerHTML = netImage;
       document.getElementById("demo").innerHTML =
         "Hi " +
         myName.value +
         ", your network  service provider is Expresso Ghana";
     } else if (
-      (myCountry.value =
-        "cam" & text.includes("745") ||
+      (myCountry.value === "cam") &
+      (text.includes("745") ||
         text.includes("746") ||
         text.includes("747") ||
         text.includes("75") ||
         text.includes("77") ||
         text.includes("748"))
     ) {
-      let netImage = `<img src="https://res.cloudinary.com/seyitunde/image/upload/v1658828503/ninemobile.png" alt="9mobile logo" id="ninemobile-logo"></img>`;
+      let netImage = `<img src=".//images/mtn.png" alt="mtn logo" id="mtn-logo"></img>`;
       container.innerHTML = netImage;
       document.getElementById("demo").innerHTML =
         "Hi " +
         myName.value +
         ", your network  service provider is MTN Cameroon";
     } else if (
-      (myCountry.value =
-        "cam" & text.includes("940") ||
+      (myCountry.value === "cam") &
+      (text.includes("940") ||
         text.includes("941") ||
         text.includes("942") ||
         text.includes("943") ||
@@ -138,22 +167,23 @@ function startApp() {
         text.includes("99") ||
         text.includes("96"))
     ) {
-      let netImage = `<img src="https://res.cloudinary.com/seyitunde/image/upload/v1658828503/ninemobile.png" alt="9mobile logo" id="ninemobile-logo"></img>`;
+      let netImage = `<img src=".//images/oran.png" alt="9mobile logo"></img>`;
       container.innerHTML = netImage;
       document.getElementById("demo").innerHTML =
         "Hi " +
         myName.value +
         ", your network  service provider is Orange Cameroon";
     } else if (
-      (myCountry.value = "cam" & text.includes("22") || text.includes("33"))
+      (myCountry.value === "cam") &
+      (text.includes("22") || text.includes("33"))
     ) {
       let netImage = `<img src="https://res.cloudinary.com/seyitunde/image/upload/v1658828503/ninemobile.png" alt="9mobile logo" id="ninemobile-logo"></img>`;
       container.innerHTML = netImage;
       document.getElementById("demo").innerHTML =
         "Hi " + myName.value + ", your network  service provider is CAMTEL";
     } else if (
-      (myCountry.value =
-        "ken" & text.includes("11") ||
+      (myCountry.value === "ken") &
+      (text.includes("11") ||
         text.includes("701") ||
         text.includes("702") ||
         text.includes("703") ||
@@ -199,8 +229,8 @@ function startApp() {
         myName.value +
         ", your network  service provider is Safaricom Kenya";
     } else if (
-      (myCountry.value =
-        "ken" & text.includes("10") ||
+      (myCountry.value === "ken") &
+      (text.includes("10") ||
         text.includes("731") ||
         text.includes("732") ||
         text.includes("733") ||
@@ -237,31 +267,35 @@ function startApp() {
         "Hi " +
         myName.value +
         ", your network  service provider is Airtel Kenya";
-      } else if (
-        (myCountry.value =
-          "ken" & text.includes("940") ||
-          text.includes("771") ||
-          text.includes("772") ||
-          text.includes("773") ||
-          text.includes("774") ||
-          text.includes("775") ||
-          text.includes("776") ||
-          text.includes("777") ||
-          text.includes("778") ||
-          text.includes("779"))
-      ) {
-        let netImage = `<img src="https://res.cloudinary.com/seyitunde/image/upload/v1658828503/ninemobile.png" alt="9mobile logo" id="ninemobile-logo"></img>`;
-        container.innerHTML = netImage;
-        document.getElementById("demo").innerHTML =
-          "Hi " +
-          myName.value +
-          ", your network  service provider is Telkom Kenya";
+    } else if (
+      (myCountry.value === "ken") &
+      (text.includes("940") ||
+        text.includes("771") ||
+        text.includes("772") ||
+        text.includes("773") ||
+        text.includes("774") ||
+        text.includes("775") ||
+        text.includes("776") ||
+        text.includes("777") ||
+        text.includes("778") ||
+        text.includes("779"))
+    ) {
+      let netImage = `<img src="https://res.cloudinary.com/seyitunde/image/upload/v1658828503/ninemobile.png" alt="9mobile logo" id="ninemobile-logo"></img>`;
+      container.innerHTML = netImage;
+      document.getElementById("demo").innerHTML =
+        "Hi " +
+        myName.value +
+        ", your network  service provider is Telkom Kenya";
     } else {
       document.getElementById("demo").innerHTML = "Invalid service provider";
       container.innerHTML = "";
       console.log("invalid");
     }
   });
+  button1.addEventListener("click", function (e){ 
+    netForm.style.display = 'block'
+    netResult.style.display = 'none'
+   })
 }
 
 // ======= DO NOT EDIT ============== //
